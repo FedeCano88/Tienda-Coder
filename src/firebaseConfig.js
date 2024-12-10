283fcf9acc81a3c0847b22ebf73a24a4ffcc8875
FedericoCano <fedecano1988@gmail.com>
2024-12-09 18:31:04 -0600

    correccion firebase .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,14 @@
-// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDVoG9YQGAsKwm5cwqLEGfDNbD0uvaDDcw",
-    authDomain: "tienda-coder-533fd.firebaseapp.com",
-    projectId: "tienda-coder-533fd",
-    storageBucket: "tienda-coder-533fd.firebasestorage.app",
-    messagingSenderId: "516441227093",
-    appId: "1:516441227093:web:3df915b809d341fafcf9dc",
-    measurementId: "G-G3ZR7PYWHM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
